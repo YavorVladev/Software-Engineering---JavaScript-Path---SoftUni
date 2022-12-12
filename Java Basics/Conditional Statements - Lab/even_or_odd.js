918da6c1b00aa3770cc7d63daab00ea65e1275da
YavorVladev <boxmmox@gmail.com>
2022-12-12 23:52:59 +0200

    JavaScript Basics
    ref

---
+++ Java Basics/Conditional Statements - Lab/even_or_odd.js
@@ -0,0 +1,10 @@
+function even_or_odd(input){
+    let number = Number(input[0]);
+    if (number % 2 == 0) {
+        console.log("even");
+    } else {
+        console.log("odd");
+    }
+}
+
+even_or_odd(["2"])
