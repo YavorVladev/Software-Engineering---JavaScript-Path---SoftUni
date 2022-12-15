7b84113769b439aec21d6fe8cfd1777a0b6d1723
YavorVladev <boxmmox@gmail.com>
2022-12-15 16:28:26 +0200

    JavaScript Basics
    ref

--- Java Basics/Conditional Statements Advanced - Lab/trade_commissions.js
+++ Java Basics/Conditional Statements Advanced - Lab/trade_commissions.js
@@ -13,55 +13,43 @@ function solve(input){
     if (0 <= sales && sales <= 500) {
         switch (town) {
             case "Sofia":
-                price = sales * 0.05; console.log(price.toFixed(2));
-                break;
+                price = sales * 0.05; console.log(price.toFixed(2)); break;
             case "Varna":
-                price = sales * 0.045; console.log(price.toFixed(2));
-                break;
+                price = sales * 0.045; console.log(price.toFixed(2)); break;
             case "Plovdiv":
-                price = sales * 0.055; console.log(price.toFixed(2));
-                break;
+                price = sales * 0.055; console.log(price.toFixed(2)); break;
         }
 
     } else if (500 < sales && sales <= 1000) {
         switch (town) {
             case "Sofia":
-                price = sales * 0.07; console.log(price.toFixed(2));
-                break;
+                price = sales * 0.07; console.log(price.toFixed(2)); break;
             case "Varna":
-                price = sales * 0.075; console.log(price.toFixed(2));
-                break;
+                price = sales * 0.075; console.log(price.toFixed(2)); break;
             case "Plovdiv":
-                price = sales * 0.08; console.log(price.toFixed(2));
-                break;
+                price = sales * 0.08; console.log(price.toFixed(2)); break;
         }
     } else if (1000 < sales && sales <= 10000) {
         switch (town) {
             case "Sofia":
-                price = sales * 0.08; console.log(price.toFixed(2));
-                break;
+                price = sales * 0.08; console.log(price.toFixed(2)); break;
             case "Varna":
-                price = sales * 0.10; console.log(price.toFixed(2));
-                break;
+                price = sales * 0.10; console.log(price.toFixed(2)); break;
             case "Plovdiv":
-                price = sales * 0.12; console.log(price.toFixed(2));
-                break;
+                price = sales * 0.12; console.log(price.toFixed(2)); break;
         }
     } else if (sales > 10000) {
         switch (town) {
             case "Sofia":
-                price = sales * 0.12; console.log(price.toFixed(2));
-                break;
+                price = sales * 0.12; console.log(price.toFixed(2)); break;
             case "Varna":
-                price = sales * 0.13; console.log(price.toFixed(2));
-                break;
+                price = sales * 0.13; console.log(price.toFixed(2)); break;
             case "Plovdiv":
-                price = sales * 0.145; console.log(price.toFixed(2));
-                break;
+                price = sales * 0.145; console.log(price.toFixed(2)); break;
         }
     }
 
 }
 
-solve(["Kaspichan",
-"-50"])
+solve(["Varna",
+"3874.50"])
