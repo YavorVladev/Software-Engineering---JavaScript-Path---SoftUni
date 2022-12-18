efbe85ced79fe759d3ec353eaa15b673c83e2436
YavorVladev <boxmmox@gmail.com>
2022-12-19 00:26:01 +0200

    JavaScript Basics
    ref

---
+++ Java Basics/While Loop - Lab/password.js
@@ -0,0 +1,23 @@
+function solve(input){
+
+    let index = 0
+    let username = input.shift();
+    let password = input.shift();
+
+    while (true) {
+
+        let current_password = input[index];
+        index ++;
+
+        if (current_password == password) {
+            console.log(`Welcome ${username}`)
+            break;
+        }
+
+    }
+}
+
+solve(["Gosho",
+"secret",
+"secret"])
+
