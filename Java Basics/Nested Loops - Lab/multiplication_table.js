d11b66351e9fbf96b342be23000bb7676021540e
YavorVladev <boxmmox@gmail.com>
2022-12-22 22:05:51 +0200

    JavaScript Basics
    ref

---
+++ Java Basics/Nested Loops - Lab/multiplication_table.js
@@ -0,0 +1,11 @@
+function solve() {
+
+    for (let f = 1; f <= 10; f ++) {
+        for (let s = 1; s <= 10; s ++) {
+            let result = f * s;
+            console.log(`${f} * ${s} = ${result}`)
+        }
+    }
+}
+
+solve()
