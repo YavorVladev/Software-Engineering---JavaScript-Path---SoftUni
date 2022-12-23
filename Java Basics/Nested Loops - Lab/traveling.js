54ff8d9e918b25f2ef4cff48ac3b8583b7e23507
YavorVladev <boxmmox@gmail.com>
2022-12-23 21:06:01 +0200

    JavaScript Basics
    ref

---
+++ Java Basics/Nested Loops - Lab/traveling.js
@@ -0,0 +1,40 @@
+function solve(input) {
+
+    let destination = input.shift();
+
+    while (destination !== "End") {
+        let needed_money = Number(input.shift());
+        let saved_money = 0;
+        while (saved_money < needed_money) {
+            let saved_amount = Number(input.shift());
+            saved_money += saved_amount;
+        }
+
+
+        console.log(`Going to ${destination}`)
+        destination = input.shift();
+    }
+}
+solve(["France",
+"2000",
+"300",
+"300",
+"200",
+"400",
+"190",
+"258",
+"360",
+"Portugal",
+"1450",
+"400",
+"400",
+"200",
+"300",
+"300",
+"Egypt",
+"1900",
+"1000",
+"280",
+"300",
+"500",
+"End"])
