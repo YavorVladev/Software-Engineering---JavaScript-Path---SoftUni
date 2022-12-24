7adda37f3b9609636f3a6ca5b3b224c7a3ba002c
YavorVladev <boxmmox@gmail.com>
2022-12-24 23:46:34 +0200

    JavaScript Basics
    ref

---
+++ Java Basics/Nested Loops - Exercise/number_pyramid.js
@@ -0,0 +1,35 @@
+function solve ([arg1]) {
+
+    let n = Number(arg1)
+
+    let num = 1;
+
+    for(let row = 1; row <= n; row++) {
+
+        for(let col = 1; col <= row; col++){
+
+            process.stdout.write(num + " ")
+
+            num++;
+
+            if (num > n) {
+
+                break;
+
+            }
+
+        }
+
+        console.log();
+      if (num > n) {
+
+                break;
+
+            }
+
+
+    }
+
+}
+
+solve([10])
