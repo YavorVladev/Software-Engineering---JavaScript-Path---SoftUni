c13cb3c06a3189253b9c6b78fdcd1425f2c30556
YavorVladev <boxmmox@gmail.com>
2023-01-06 15:20:22 +0200

    JavaScript Fundamentals
    ref

---
+++ JavaScript Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/foreign_languages.js
@@ -0,0 +1,17 @@
+function getLanguage(country) {
+    let language;
+    switch (country) {
+      case "England":
+      case "USA":
+        language = "English";
+        break;
+      case "Spain":
+      case "Argentina":
+      case "Mexico":
+        language = "Spanish";
+        break;
+      default:
+        language = "unknown";
+    }
+    console.log(language);
+  }
