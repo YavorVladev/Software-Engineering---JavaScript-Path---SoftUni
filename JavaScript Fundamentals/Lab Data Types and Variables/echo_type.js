567975dfc057a90b0c2c9fac584043855802d0bc
YavorVladev <boxmmox@gmail.com>
2023-01-30 22:49:06 +0200

    JavaScript Fundamentals
    ref

---
+++ JavaScript Fundamentals/Lab Data Types and Variables/echo_type.js
@@ -0,0 +1,12 @@
+function solve(data) {
+
+    console.log(typeof(data))
+    if (data !== null){
+        console.log(data)
+    } else {
+        console.log(`Parameter is not suitable for printing`)
+
+    }
+}
+
+solve("Hello")
