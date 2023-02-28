33cd54de2828f4d29d6b5b1cf5cfc3a19f17d5e7
YavorVladev <boxmmox@gmail.com>
2023-02-28 20:13:21 +0200

    JavaScript Fundamentals
    ref

---
+++ JavaScript Fundamentals/Data Types and Variables - More Exercise/digitsWithWords.js
@@ -0,0 +1,16 @@
+function digitWithWords(digitWord) {
+  switch (digitWord) {
+    case "one": console.log(Number(1)); break;
+    case "two":
+    case "three":
+    case "four":
+    case "five":
+    case "six":
+    case "seven":
+    case "eight":
+    case "nine":
+    case "0":
+  }
+}
+
+digitWithWords("one")
