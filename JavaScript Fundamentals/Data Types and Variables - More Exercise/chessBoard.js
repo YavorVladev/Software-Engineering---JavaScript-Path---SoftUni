0654897a58f788df29e0ba82bca6b13c53dd81aa
YavorVladev <boxmmox@gmail.com>
2023-03-03 12:33:11 +0200

    JavaScript Fundamentals
    ref

---
+++ JavaScript Fundamentals/Data Types and Variables - More Exercise/chessBoard.js
@@ -0,0 +1,18 @@
+function printChessboard(n) {
+    let result = '<div class="chessboard">\n';
+    for (let i = 0; i < n; i++) {
+        result += '  <div>\n';
+        for (let j = 0; j < n; j++) {
+            if ((i + j) % 2 === 0) {
+                result += '    <span class="black"></span>\n';
+            } else {
+                result += '    <span class="white"></span>\n';
+            }
+        }
+        result += '  </div>\n';
+    }
+    result += '</div>';
+    return result;
+}
+
+console.log(printChessboard(3))
